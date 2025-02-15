9cca9f466d322f0b5a6acabe7d958247fae03258
lammai442 <lam.mai442@gmail.com>
2025-02-15 10:45:45 +0100

    Added comment and fixed correct address path

--- template/scripts/script.js
+++ template/scripts/script.js
@@ -1,14 +1,26 @@
+import { oData } from './data/data.js';
+import { fetchTopMovies } from './modules/api.js';
 
-if(window.location.pathname === '/' || window.location.pathname === '/index.html') {
+
+if(window.location.pathname === '/' || window.location.pathname === '/template/index.html') {
     console.log('index.html');
-
-} else if(window.location.pathname === '/favorites.html') {
     console.log('favorites.html');
+    console.log('hej');
+    console.log(oData.topMovieList);
+    async function init() {
+        await fetchTopMovies();
+        console.log(oData);
+    }
+    init();
 
-} else if(window.location.pathname === '/movie.html') {
+} else if(window.location.pathname === '/template/favorites.html') {
+  
+} else if(window.location.pathname === '/template/movie.html') {
     console.log('movie.html');
 
-} else if(window.location.pathname === '/search.html') {
+} else if(window.location.pathname === '/template/search.html') {
     console.log('search.html');
 
 }
+
+
