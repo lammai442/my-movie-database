c27c6641601b7a4e1c8f048e227e9290b8f128d3
lammai442 <lam.mai442@gmail.com>
2025-03-27 20:06:40 +0100

    Logs

--- scripts/script.js
+++ scripts/script.js
@@ -9,15 +9,19 @@ if (
 	window.location.pathname === '/' ||
 	window.location.pathname === '/index.html'
 ) {
+	console.log('index');
+
 	indexPageSetup();
 }
 
 // Favorites-sidan
 else if (window.location.pathname === '/favorites.html') {
+	console.log('favorites');
 	favouritePageSetup();
 }
 
 // Search-sidan
 else if (window.location.pathname === '/search.html') {
+	console.log('search');
 	searchPageSetup();
 }
